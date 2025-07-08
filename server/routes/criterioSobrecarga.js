243eb601ec60e517b8201c00952cd1b545593d05
Lucas Ferreira <182416116+lcafe@users.noreply.github.com>
2025-07-08 11:46:07 -0300

    Alteração criterioSobrecarga.js
    Estava com variável I2 que não existe mais, isto estava causando erros no cálculo.

--- server/routes/criterioSobrecarga.js
+++ server/routes/criterioSobrecarga.js
@@ -6,22 +6,21 @@ const { criterioSobrecarga } = require('../../lib/criterioSobrecarga');
 
 // Rota POST para avaliar o critério de sobrecarga
 router.post('/criterio-sobrecarga', (req, res) => {
-  const { Ib, Ins, Iz, I2, condCemHoras } = req.body;
+  const { Ib, Ins, Iz, condCemHoras } = req.body;
 
   if (
     typeof Ib !== 'number' ||
     typeof Ins !== 'number' ||
     typeof Iz !== 'number' ||
-    typeof I2 !== 'number' ||
     typeof condCemHoras !== 'boolean'
   ) {
     return res.status(400).json({
-      erro: 'Parâmetros inválidos. Envie Ib, Ins, Iz, I2 (números) e condCemHoras (booleano).'
+      erro: 'Parâmetros inválidos. Envie Ib, Ins, Iz e condCemHoras (booleano).'
     });
   }
 
   try {
-    const resultado = criterioSobrecarga(Ib, Ins, Iz, I2, condCemHoras);
+    const resultado = criterioSobrecarga(Ib, Ins, Iz, condCemHoras);
     return res.json({
       resultado
     });
